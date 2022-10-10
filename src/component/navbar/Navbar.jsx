0021bcf9073a1be51348dbd71ffa9e06bcd6d969
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-10-10 23:04:16 +0200

    Added icons to navbar with some scss

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,7 +1,43 @@
 import "./navbar.scss"
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
+import ChatIcon from '@mui/icons-material/Chat';
+import ListIcon from '@mui/icons-material/List';
+
 
 export default function Navbar() {
   return (
-    <div className="navbar">Navbar</div>
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageIcon />
+            English
+          </div>
+          <div className="item">
+            <DarkModeIcon />
+          </div>
+          <div className="item">
+            <FullscreenIcon />
+          </div>
+          <div className="item">
+            <CircleNotificationsIcon />
+          </div>
+          <div className="item">
+            <ChatIcon />
+          </div>
+          <div className="item">
+            <ListIcon />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import "./home.scss"
 import Sidebar from "../../component/sidebar/Sidebar"
+import Navbar from "../../component/navbar/Navbar"
 
 const Home = () => {
     return (
         <div className="home">
             <Sidebar/>
-            <div className="homeContainer">container</div>
+            <div className="homeContainer">
+                <Navbar/>
+                Home Container
+                </div>
         </div>
     )
 }
