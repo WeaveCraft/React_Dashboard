27f2e29e6270c45b880a94a9b7cecb9abd0e5590
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-10-11 23:00:59 +0200

    Featured is complete.

--- src/component/featured/Featured.jsx
+++ src/component/featured/Featured.jsx
@@ -2,6 +2,9 @@ import "./featured.scss"
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { CircularProgressbar } from "react-circular-progressbar"
 import "react-circular-progressbar/dist/styles.css"
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
 
 const Featured = () => {
   return (
@@ -18,7 +21,27 @@ const Featured = () => {
         <p className="amount">$1252</p>
         <p className="desc">Amount spent is considered above average.</p>
         <div className="summary">
-          <div className="item"></div>
+          <div className="item">
+            <div className="itemTitle">Maximum</div>
+            <div className="itemResult negative">
+              <SentimentVeryDissatisfiedIcon fontSize="small" />
+              <div className="resultAmount">$2.750</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Medium</div>
+            <div className="itemResult">
+              <SentimentSatisfiedIcon fontSize="small" />
+              <div className="resultAmount">$2.150</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Low</div>
+            <div className="itemResult positive">
+              <SentimentSatisfiedAltIcon fontSize="small" />
+              <div className="resultAmount">$1.500</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
